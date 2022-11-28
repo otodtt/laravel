d89c0a3244dd4f84315f6c67bcade02d80654efe
otodtt <dtenevoto@mail.bg>
2022-11-28 17:24:17 +0200

    28.11.22 sl

---
+++ resources/views/farmers/body/qprotocols.blade.php
@@ -0,0 +1,14 @@
+<fieldset class="new_opinions">
+    <legend class="legend_apt">СЕРТИФИКАТИ - Сертификати по качество на ППЗ издадени на Земеделския Стопанин</legend>
+    @foreach($qcertificates as $qcertificate)
+        <div class="col-md-6" >
+            <p class="">
+                Сертификат с &#8470; <span class="bold">{{$qcertificate->internal}}</span>
+                издаен на <span class="bold">{{date('d.m.Y', $qcertificate->date_issue)}}</span> г.
+            </p>
+        </div>
+        <div class="col-md-5" >
+            <a class="fa fa-binoculars btn btn-info my_btn" href="/контрол/сертификати-вътрешен/{{$qcertificate->id}}" style="margin: 3px 0"> Виж Сертификата!</a>
+        </div>
+    @endforeach
+</fieldset>
