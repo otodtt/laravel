58acb281022eaf05b64279d910ba6231ca140f6e
otodtt <dtenevoto@mail.bg>
2022-12-23 14:26:02 +0200

    23.12.22 sl

---
+++ resources/views/useful/edit/edit.blade.php
@@ -0,0 +1,118 @@
+@extends('layouts.useful')
+@section('title')
+    {{ 'Добавяне на документ' }}
+@endsection
+
+@section('css')
+{{--    {!!Html::style("css/firms_objects/firms_all_css.css" )!!}--}}
+    {{--{!!Html::style("css/table/jquery.dataTables.css" )!!}--}}
+    {!!Html::style("css/table/table_firms.css " )!!}
+{{--    {!!Html::style("css/sb-admin-2.css" )!!}--}}
+    {!!Html::style("css/admin/admin.css" )!!}
+    {!!Html::style("css/admin/verifications.css" )!!}
+    {{--{!!Html::style("css/date/jquery.datetimepicker.css" )!!}--}}
+@endsection
+
+@section('message')
+    @include('layouts.alerts.success')
+@endsection
+
+@section('content')
+    <div class="div-layout-title" style="margin-bottom: 20px; margin-top: 20px">
+        <h4 class="bold layout-title">ДОБАВЯНЕ НА ДОКУМЕНТ</h4>
+    </div>
+    <hr/>
+    <div class="btn-group">
+        <a href="/" class="fa fa-home btn btn-info my_btn"> Началo</a>
+        {{--<span class="fa  btn btn-default my_btn"><i class="fa fa-euro " aria-hidden="true"></i>  Регламенти</span>--}}
+        <a href="{!! URL::to('/полезно/регламенти')!!}" class="fa fa-euro btn btn-info my_btn"> Регламенти</a>
+        <a href="{!! URL::to('/полезно/закони')!!}" class="fa fa-balance-scale btn btn-info my_btn"> Закони</a>
+        <a href="{!! URL::to('/полезно/наредби')!!}" class="fa fa-gavel btn btn-info my_btn"> Наредби</a>
+        <a href="{!! URL::to('/полезно/Бланки')!!}" class="fa fa-tags btn btn-info my_btn"> Бланки</a>
+    </div>
+    {{--<div class="btn_add_firm">--}}
+        {{--<a href="{!!URL::to('/полезно/добави-документ')!!}" class="fa fa-arrow-circle-right btn btn-danger my_btn">--}}
+            {{--Добави Документ--}}
+        {{--</a>--}}
+    {{--</div>--}}
+    {{--<hr/>--}}
+    {{--<div class="btn-group" >--}}
+        {{--<span class="fa fa-arrow-down btn btn-default my_btn"> Сетификати/Внос</span>--}}
+        {{--<a href="{!! URL::to('/контрол/сертификати-износ')!!}" class="fa fa-arrow-up btn btn-info my_btn"> Сетификати/Износ</a>--}}
+        {{--<a href="{!! URL::to('/контрол/сертификати-вътрешен')!!}" class="fa fa-retweet btn btn-info my_btn"> Вътрешни</a>--}}
+    {{--</div>--}}
+    <hr/>
+    <fieldset class="form-group">
+        <div class="wrap_sort">
+
+        </div>
+    </fieldset>
+    <hr/>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="alert alert-success alert-dismissible">
+                        <p class="description_alert"><span class="red">ВНИМАНИЕ!</span> Тук се описват всички проверки на ЗС за които се издава Констативен Протокол!<br/>
+                        <p class="description_alert"><span class="red">ВНИМАНИЕ!</span> Не се описват проверките на Агроаптеките и Нерегламентираните обекти<br/>
+                            1. Изпиши пълното име на Проверката!<br/>
+                            2. Изпиши съкратено име!<br/>
+
+                        </p>
+                    </div>
+                    <div class="panel-body">
+                        @if(count($errors)>0)
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error  }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        {!! Form::open(['url'=>'useful/document/store', 'method'=>'POST', 'id'=>'form', 'files'=>true]) !!}
+
+                            @include('useful.create.form')
+                            <br/>
+                            <div class="form-group">
+                                <label class="col-md-5 control-label">Избери файл</label>&nbsp;&nbsp;
+                                <div class="col-md-6">
+                                    {!! Form::file('blade',['id'=>'filename']) !!}
+                                </div>
+                                <br/>
+                            </div>
+                            <br/>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">Добави!</button>
+                                </div>
+                            </div>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--<div class="btn_add_certificate" style="text-align: right">--}}
+        {{--<a href="{!! URL::to('/контрол/сертификати-внос') !!}" class="fa fa-eraser btn btn-primary my_btn right_btn">--}}
+            {{--&nbsp; Изчисти сортирането!--}}
+        {{--</a>--}}
+    {{--</div>--}}
+    {{--<hr/>--}}
+{{--    @include('quality.certificates.includes.table')--}}
+@endsection
+
+@section('scripts')
+    {{--{!!Html::script("js/table/jquery-1.11.3.min.js" )!!}--}}
+    {{--{!!Html::script("js/table/jquery.dataTables.js" )!!}--}}
+    {{--{!!Html::script("js/quality/QcertificatesTable.js" )!!}--}}
+
+    {{--{!!Html::script("js/build/jquery.datetimepicker.full.min.js" )!!}--}}
+    {{--{!!Html::script("js/date/in_date.js" )!!}--}}
+    {{--<script>--}}
+        {{--var selectedVal = $("#years option:selected").val();--}}
+        {{--var getYear = document.getElementById("get_year").value = selectedVal;--}}
+    {{--</script>--}}
+@endsection
---
+++ resources/views/useful/edit/form.blade.php
@@ -0,0 +1,72 @@
+<div class="form-group">
+    <label class="col-md-4 control-label">Избери Вида на Документа</label>
+    <div class="col-md-6">
+{{--        {!! Form::label('place', 'Инспектор:', ['class'=>'my_labels']) !!}--}}
+        <select name="document_type" id="document_type" class="localsID form-control" style=" margin: 5px 0; width: 200px; display: inline-block">
+            <option value="0">-- Избери --</option>
+            <option value="1" {{(old('document_type') == 1)? 'selected':''}}>Регламент / Директива</option>
+            <option value="2" {{(old('document_type') == 2)? 'selected':''}}>Закон</option>
+            <option value="3" {{(old('document_type') == 3)? 'selected':''}}>Наредба</option>
+            <option value="4" {{(old('document_type') == 4)? 'selected':''}}>Бланка</option>
+        </select>
+    </div>
+    <br/>
+</div>
+<br/>
+<div class="form-group">
+    <label class="col-md-4 control-label">Цялото име на документа</label>
+    <div class="col-md-6">
+        {!! Form::text('document_name', null, ['class'=>'form-control', 'placeholder'=>'Цялото име']) !!}
+    </div>
+    <br/>
+</div>
+<br/>
+<div class="form-group">
+    <label class="col-md-4 control-label">Кратко име</label>
+    <div class="col-md-6">
+        {!! Form::text('document_short', null, ['class'=>'form-control', 'placeholder'=>'Кратко име']) !!}
+    </div>
+    <br/>
+</div>
+<hr/>
+<div class="radio_buttons">
+    <table class="radio_table">
+        <tbody>
+            <tr>
+                <td class="buttons_radio"><label class="col-md-4 control-label my_label">Всички</label></td>
+                <td>&nbsp;&nbsp;{!! Form::radio('document_for', 0, false) !!} </td>
+            </tr>
+            <tr>
+                <td class="buttons_radio"><label class="col-md-4 control-label my_label">Контрол</label></td>
+                <td>&nbsp;&nbsp;{!! Form::radio('document_for', 1, false) !!}</td>
+            </tr>
+            <tr>
+                <td class="buttons_radio"><label class="col-md-4 control-label my_label">ФСК</label></td>
+                <td>&nbsp;&nbsp;{!! Form::radio('document_for', 2, false) !!}</td>
+            </tr>
+            <tr>
+                <td class="buttons_radio"><label class="col-md-4 control-label my_label">КППЗ</label></td>
+                <td>&nbsp;&nbsp;{!! Form::radio('document_for', 3, false) !!}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="radio_description">
+    <p class="bold">Задължително избери една от следните възможности!</p>
+    <p>В кой отдел ще се ползва документа</p>
+</div>
+<hr/>
+<div class="form-group">
+    <br/>
+    {!! Form::label('new', 'Действаш ли е документа?', ['class'=>'col-md-4 control-label']) !!}
+    <div class="col-md-6">
+        <label><span>&nbsp;&nbsp;Да: </span>
+            {!! Form::radio('is_active', 1, true) !!}
+        </label>
+        <label><span>&nbsp;&nbsp;Не: </span>
+            {!! Form::radio('is_active', 0, false) !!}
+        </label>
+    </div>
+</div>
+<hr/>
